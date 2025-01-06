06d078bb312bda352a843a735c5dddc5130eeb28
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-01-07 01:01:30 +0530

    RM6785-common: Switch to OSS Mediatek AIDL PowerHAL Implementation

--- RM6785.mk
+++ RM6785.mk
@@ -233,20 +233,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
-    android.hardware.power@1.2.vendor:64
-
-PRODUCT_PACKAGES += \
-    libmtkperf_client_vendor \
-    libmtkperf_client
-
-PRODUCT_PACKAGES += \
-    vendor.mediatek.hardware.mtkpower@1.0.vendor \
-    vendor.mediatek.hardware.mtkpower@1.1.vendor \
-    vendor.mediatek.hardware.mtkpower@1.2-service.stub
+    android.hardware.power-service-mediatek \
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/power,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # PowerOffAlarm
 PRODUCT_PACKAGES += \
@@ -292,10 +283,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/mediatek \
-    hardware/mediatek/libmtkperf_client \
-    $(LOCAL_PATH)
-
+    hardware/mediatek 
+	
 # Soundtrigger
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.3-impl:32
