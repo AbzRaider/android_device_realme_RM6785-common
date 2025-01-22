202b9fb7c3037a8311e7e9c764e2a80ad6cab8e5
Saikrishna1504 <saikrishna26918@gmail.com>
2025-01-22 23:24:34 +0000

    RM6785-common: Build more missing VNDK blobs E APUWareServer: unable to open Apusys library APUWareApusysServer.so, with error dlopen failed: library "APUWareApusysServer.so" not found

--- RM6785.mk
+++ RM6785.mk
@@ -152,9 +152,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.memtrack-service.mediatek-mali
 
-# Neutral Networks
+# Neural Networks
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.3.vendor:64
+    android.hardware.neuralnetworks@1.3.vendor:64 \
+    APUWareApusysServer.vendor
 
 # NFC
 PRODUCT_PACKAGES += \
