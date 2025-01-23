847a3b01172aa13c8c0131d4deb8ca0a5f0fe4af
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-01-23 17:35:47 +0000

    [DNM] fuck this

--- RM6785.mk
+++ RM6785.mk
@@ -237,10 +237,9 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
-    android.hardware.power@1.2.vendor \
-    vendor.mediatek.hardware.mtkpower@1.0 \
-    vendor.mediatek.hardware.mtkpower@1.1 \
-    vendor.mediatek.hardware.mtkpower@1.2
+    android.hardware.power@1.2.vendor:64 \
+    vendor.mediatek.hardware.mtkpower@1.2.vendor:64 \
+    vendor.mediatek.hardware.mtkpower@1.0.vendor:32	
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/power,$(TARGET_COPY_OUT_VENDOR)/etc)
