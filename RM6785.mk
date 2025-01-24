7f941ffe1adf01c273b11667908b8689aaffb8a7
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2025-01-24 13:00:14 +0000

    RM6785-common: Compile more 32bit MTKPower Services

--- RM6785.mk
+++ RM6785.mk
@@ -258,6 +258,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
     android.hardware.power@1.2.vendor:64 \
     vendor.mediatek.hardware.mtkpower@1.2.vendor:64 \
+    vendor.mediatek.hardware.mtkpower@1.2.vendor:32 \
     vendor.mediatek.hardware.mtkpower@1.0.vendor:32	
 
 PRODUCT_COPY_FILES += \
